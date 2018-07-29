cec6c163437fdde45e5ab92f0e348476cd27d3ce
gopovesteban <36285981+gopovesteban@users.noreply.github.com>
2018-07-29 12:47:52 -0600

    Examen pt 1
    pt 1 criba

---
+++ Criba/src/CribaETest/CribaETest.java
@@ -0,0 +1,14 @@
+package CribaETest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CribaETest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
